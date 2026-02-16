4a39d2323b2b8c051413efd973336ab0bf6ddf52
bluemzzn <synthiawithsyren@gmail.com>
2026-02-16 09:21:33 +0700

    p01 2

--- src/practicals/p01/index.ts
+++ src/practicals/p01/index.ts
@@ -1,6 +1,6 @@
 import { Student } from './student'
 
-const student = new Student('firstname', 'lastname')
+const student = new Student()
 
 student.firstname = 'John'
 student.lastname = 'Doe'
--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,12 +1,10 @@
 export class Student {
-  firstname: string;
-  lastname: string;
-  constructor(firstname: string, lastname: string) {
-    this.firstname = firstname;
-    this.lastname = lastname;
-  }
+  constructor(
+    public firstname: string,
+    public lastname: string
+  ) {}
 
-  printName(): string{
-    return `${this.firstname} ${this.lastname}`
+  printName(): string {
+    return `${this.firstname} ${this.lastname}`;
   }
 }
