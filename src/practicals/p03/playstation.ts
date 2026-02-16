94aed05fa11de31699eeeddcf3b3b4357c62c5c6
bluemzzn <synthiawithsyren@gmail.com>
2026-02-16 10:00:29 +0700

    p03 error

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -2,4 +2,19 @@ import { Product } from './product'
 
 export class Playstation extends Product {
   // TODO: implement class properties, constructor with super(...), and methods
+  static readonly DISCOUNT_PERCENT: number = 10;
+  public generation : number = 0; 
+
+  constructor(name : string, generation : number = 0){
+    super(name,price);
+    this.generation = generation;
+  }
+
+  getProfile(name : string){
+    return `${this.name} (Gen ${this.generation})`;
+  }
+  getDiscountPrice(){
+    const discount = this.getPrice() * (Playstation.DISCOUNT_PERCENT / 100);
+    return this.getPrice();
+  }
 }
--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,2 +1,13 @@
-export class Product {}
+export class Product {
+  public readonly name: string = "";
+  private price: number = 0;
+  constructor(name?: string, price?: number) {}
 
+  getPrice(): number {
+    return this.price;
+  }
+
+  setPrice(price: number): void {
+    this.price = price;
+  }
+}
