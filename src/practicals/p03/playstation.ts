dc751f35747de42f1fc773317cb7538440d79815
bluemzzn <synthiawithsyren@gmail.com>
2026-02-16 10:14:39 +0700

    last

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -2,10 +2,9 @@ import { Product } from './product'
 
 export class Playstation extends Product {
   // TODO: implement class properties, constructor with super(...), and methods
-  static readonly DISCOUNT_PERCENT: number = 10;
   public generation : number = 0; 
 
-  constructor(name : string, generation : number = 0, price: number = 0){
+  constructor(name : string = '', generation : number = 0, price: number = 0){
     super(name,price);
     this.generation = generation;
   }
@@ -14,7 +13,7 @@ export class Playstation extends Product {
     return `${this.name} (Gen ${this.generation})`;
   }
   getDiscountPrice(): number{
-    const discount = this.getPrice() * (Playstation.DISCOUNT_PERCENT / 100);
+    const discount = this.getPrice() * (Product.DISCOUNT_PERCENT / 100);
     return this.getPrice() - discount;
   }
 }
