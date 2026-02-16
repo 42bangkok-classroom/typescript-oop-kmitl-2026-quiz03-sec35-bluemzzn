34167bf79ffc4f9ceca6ef53b8ecc799126bdf61
bluemzzn <synthiawithsyren@gmail.com>
2026-02-16 09:42:23 +0700

    p02 yay no error

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,17 +1,15 @@
 export class Person {
-  public firstname: string = '';
-  public lastname: string = '';
+  public firstname: string = "";
+  public lastname: string = "";
   private age: number = 0;
-  static COUNTRY : string = 'Thailand';
-  constructor(firstname: string = '', lastname: string = '', age: number = 0) {
-    this.firstname = firstname ;
+  static COUNTRY: string = "Thailand";
+  constructor(firstname: string = "", lastname: string = "", age: number = 0) {
+    this.firstname = firstname;
     this.lastname = lastname;
     this.age = age;
   }
 
-  setAge(): void {
-    this.age;
-  }
+  setAge(age: number): void {}
 
   getAge(): number {
     return this.age;
